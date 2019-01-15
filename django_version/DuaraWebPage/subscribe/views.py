b2fb784d034c624afc98762d9db3b5d03ce95a04
griffin <gmfalme@duara.io>
2019-01-15 13:10:28 +0300

    fine tune the page to be ready for production, align purple image background, use map by Don, and ensure the flash message is clean and easy to note

--- django_version/DuaraWebPage/subscribe/views.py
+++ django_version/DuaraWebPage/subscribe/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Subscribers
 from django.core.mail import send_mail
 from django.db import IntegrityError
+import json
 # from django.shortcuts impor
 # Create your views here.
 from django.core.validators import validate_email
@@ -22,7 +23,7 @@ def index(request):
             error_message = 'Invalid Email address kindy input a valid email address'
             return render(request, 'subscribe/index.html', {'error_message': error_message })
         except IntegrityError:
-            error_message = 'Email {} has already subscribed to our platform'.format(email)
+            error_message = 'You are already subscribed to our platform'
             return render(request, 'subscribe/index.html', {'error_message': error_message})
     else:
         return render(request, 'subscribe/index.html')
