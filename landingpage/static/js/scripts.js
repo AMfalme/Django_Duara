2316bb22c01f444b295ecdc0d2fa36cb35b2b36f
Griffin <gmfalme@duara.io>
2019-02-11 12:32:43 +0300

    use fontawesome cdn to ensure social media icons load and remove owl-carousel script in the script.js file which has since been deprecated and it does not play any role in the website

--- landingpage/static/js/scripts.js
+++ landingpage/static/js/scripts.js
@@ -192,21 +192,4 @@ $('#dismiss').on(
     }
 );
  } );
-		$(document).ready(function(){
-				$(".owl-carousel").owlCarousel({
-    items:4,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:2000,
-    autoplayHoverPause:true,
-    pagination:false,
-    navigation:true
-});
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000]);
-});
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay');
-});
-			});
+
