ef53263a77fb68cccb4b475bee5cbe0064466050
Cornellius Metto <ckmetto@gmail.com>
2019-02-12 15:47:35 +0300

    Change static url to 'static_files' to force re-download of static files.

--- DuaraWebPage/settings.py
+++ DuaraWebPage/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_files/'
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS=True
